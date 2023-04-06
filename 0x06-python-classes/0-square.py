7128eecc82a4954fa2c93d6a2a80efdf82427961
Osehonmoses <osehon54@gmail.com>
2023-04-06 10:42:02 -0700

    Updated

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-""" define a class called 'square' """
+""" define a class called Square. """
 class Square:
-    """To create an empty object, introduce 'pass' """
+    """To create an empty object, represent a Square by  introducing a pass."""
 	pass
