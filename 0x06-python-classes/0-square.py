01356f3e3179f756da7473810b36feb82130802a
Osehonmoses <osehon54@gmail.com>
2023-04-06 10:25:10 -0700

    Update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """This is an empty class which defines a square and it has no instance and its an empty object, so it features 'pass' that is used for empty object"""
-    pass
+    """This is an empty class and it is used to define a 'square'"""
+	pass
